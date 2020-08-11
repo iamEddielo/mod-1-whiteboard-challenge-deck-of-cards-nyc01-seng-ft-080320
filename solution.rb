fa13541503cac2e34100f941fb59f9c65bf22122
iamEddielo <iameddielopez@gmail.com>
2020-08-11 15:49:14 -0400

    initial commit

--- solution.rb
+++ solution.rb
@@ -1,7 +1,23 @@
 class Deck
 
+    attr_accessor :cards
+
+    def initialize
+        @cards = []
+
+def choose_card
+    @cards.delete_at(rand(@cards.length))
+end
+       
+
 end
 
 class Card
+    attr_reader :rank, :suit
+
+    def initialize(suit,rank)
+        @suit = suit
+        @rank = rank
+    end
 
 end
